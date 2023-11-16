e4138eaac16830008abca18df7204b64c2566672
moura <alisson_moura_rj@hotmail.com>
2023-11-16 14:02:13 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/moura/workshopmongo/resources/exception/StandarError.java
@@ -0,0 +1,66 @@
+package com.moura.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandarError implements Serializable {
+	static final long serialVersionUID = 1L;
+
+	private Long id;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+
+	public StandarError() {
+	}
+
+	public StandarError(Long id, Integer status, String error, String message, String path) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
