9f9f630cb1612165be65070f388766eb0721c9c5
moura <alisson_moura_rj@hotmail.com>
2023-11-17 16:24:31 -0300

    Inserção de usuário com POST

--- src/main/java/com/moura/workshopmongo/services/UserService.java
+++ src/main/java/com/moura/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.moura.workshopmongo.domain.User;
+import com.moura.workshopmongo.dto.UserDTO;
 import com.moura.workshopmongo.repository.UserRepository;
 import com.moura.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
